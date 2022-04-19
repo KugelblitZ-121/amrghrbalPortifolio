466675dbb4df9aeb9ee3739a50a2cf18a54a90be
KugelblitZ <amr.ghrbal@agu.edu.tr>
2022-04-19 17:11:22 +0300

    read more button

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// const head = document.querySelector('h1').textContent = 'testing'
+const app = Vue.createApp({
+  data: function () {
+    return {
+      btnText: "",
+      isRead: false,
+    };
+  },
+  methods: {
+    toggleRead: function () {
+      this.isRead = !this.isRead;
+    },
+  },
+});
+
+app.mount("#skills");
